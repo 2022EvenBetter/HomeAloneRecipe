9d876d51aac2998e96f1dc53b4624a586af41daa
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2022-11-19 21:25:19 +0900

    Create : recipeInfo.json, Feat : API.dart

--- lib/network/naver_api_connector.dart
+++ lib/network/naver_api_connector.dart
@@ -6,7 +6,9 @@ import 'package:flutter/rendering.dart';
 import 'dart:convert';
 import 'package:home_alone_recipe/constants/apiKey.dart';
 
-Future<String> getLocation() async { //경기도 성남시 수정구 창곡동
+Future<String> getLocation() async {
+  //경기도 성남시 수정구
+  LocationPermission permission = await Geolocator.requestPermission();
   Position position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high);
   //현재위치를 position이라는 변수로 저장
@@ -19,26 +21,28 @@ Future<String> getLocation() async { //경기도 성남시 수정구 창곡동
       headers: headerss);
   String jsonData = response.body;
   var myJson_dong =
-  jsonDecode(jsonData)["results"][1]['region']['area3']['name'];
-  var myJson_gu =
-  jsonDecode(jsonData)["results"][1]['region']['area2']['name'];
-  var myJson_si =
-  jsonDecode(jsonData)["results"][1]['region']['area1']['name'];
+      jsonDecode(jsonData)["results"][1]['region']['area3']['name'];
+  var myJson_gu = jsonDecode(jsonData)["results"][1]['region']['area2']['name'];
+  var myJson_si = jsonDecode(jsonData)["results"][1]['region']['area1']['name'];
 
-  String donggusi = myJson_si+" "+myJson_gu+" "+myJson_dong;
+  String donggusi = myJson_si + " " + myJson_gu + " " + myJson_dong;
 
   print("update getLocation");
   return donggusi;
 }
 
-Future <String> getLocationUrl() async{
+Future<String> getLocationUrl() async {
   Position position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high);
   //현재위치를 position이라는 변수로 저장
   String lat = position.latitude.toString();
   String lon = position.longitude.toString();
-  String apiURL='https://naveropenapi.apigw.ntruss.com/map-static/v2/raster?w=500&h=280&center='+lon+','+lat+'&level=13';
+  String apiURL =
+      'https://naveropenapi.apigw.ntruss.com/map-static/v2/raster?w=500&h=280&center=' +
+          lon +
+          ',' +
+          lat +
+          '&level=13';
   print("update getLocationURL");
   return apiURL;
 }
-
