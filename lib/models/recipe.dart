f3212580d90b7e2bd179d74180b38a0814b5f7db
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2022-11-20 14:02:54 +0900

    Feat : Recipe UI

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -5,7 +5,7 @@ class Recipe {
   String description;
   List<String> ingredients = [];
   List<String> recipe = [];
-
+  int scrapped = 0;
   Recipe(this.recipeName, this.imageURL, this.recipeCode, this.description,
       this.ingredients, this.recipe) {}
 }
