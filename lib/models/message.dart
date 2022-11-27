488b4c8b09392286e9024157764d8bcfe5e6188e
Jo0Yo0n <dlwndbs10@gmail.com>
2022-11-27 15:35:26 +0900

    Feat: update group chatting 1. UI 디자인 변경 2. 다른 사용자의 nickname 출력 3. 메시지 전송 시간 출력

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -3,11 +3,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class MessageModel {
   final String id; //해당 도큐먼트의 ID를 담기위함.
   final String content;
+  final String uid;
+  final String nickName;
   final Timestamp sendDate;
 
   MessageModel({
     this.id = '',
     this.content = '',
+    this.uid='',
+    this.nickName='',
     Timestamp? sendDate,
   }):sendDate = sendDate??Timestamp(0, 0);
 
@@ -17,6 +21,8 @@ class MessageModel {
     return MessageModel(
       id: id,
       content: map['content'] ??'',
+      uid: map['uid'] ??'',
+      nickName: map['nickName'] ??'',
       sendDate: map['sendDate']??Timestamp(0, 0)
     );
   }
@@ -24,6 +30,8 @@ class MessageModel {
   Map<String, dynamic> toMap() {
     Map<String, dynamic> data = {};
     data['content'] = content;
+    data['uid'] = uid;
+    data['nickName'] = nickName;
     data['sendDate'] = sendDate;
     return data;
   }
--- lib/screen/groupChatting.dart
+++ lib/screen/groupChatting.dart
@@ -1,8 +1,10 @@
 import 'dart:developer';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:home_alone_recipe/models/message.dart';
+import 'package:home_alone_recipe/provider/userProvider.dart';
+import 'package:home_alone_recipe/screen/chatBubble_screen.dart';
+import 'package:provider/provider.dart';
 
 class MessageListScreen extends StatefulWidget {
   const MessageListScreen({Key? key}) : super(key: key);
@@ -13,9 +15,11 @@ class MessageListScreen extends StatefulWidget {
 
 class _MessageListScreenState extends State<MessageListScreen> {
   TextEditingController controller = TextEditingController();
+  late UserProvider userProvider;
 
   @override
   Widget build(BuildContext context) {
+    userProvider = Provider.of<UserProvider>(context);
     return Scaffold(
       body: StreamBuilder<List<MessageModel>>(
         stream: streamMessages(), //중계하고 싶은 Stream을 넣는다.
@@ -36,16 +40,17 @@ class _MessageListScreenState extends State<MessageListScreen> {
                 Expanded(
                     child: ListView.builder(
                         // 상위 위젯의 크기를 기준으로 잡는게 아닌 자식위젯의 크기를 기준으로 잡음
+                        reverse: true,
                         itemCount: messages.length,
                         itemBuilder: (context, index) {
                           return ListTile(
-                            title: Text(messages[index].content),
-                            subtitle: Text(messages[index]
-                                .sendDate
-                                .toDate()
-                                .toLocal()
-                                .toString()
-                                .substring(5, 16)),
+                            title: ChatBubbles(
+                                messages[index].content,
+                                messages[index].uid.toString() ==
+                                    userProvider.uid,
+                                messages[index].nickName,
+                                messages[index].sendDate
+                            ),
                           );
                         })),
                 getInputWidget()
@@ -57,6 +62,36 @@ class _MessageListScreenState extends State<MessageListScreen> {
     );
   }
 
+  Stream<List<MessageModel>> streamMessages() {
+    try {
+      //찾고자 하는 컬렉션의 스냅샷(Stream)을 가져온다.
+      final Stream<QuerySnapshot> snapshots = FirebaseFirestore.instance
+          .collection(
+              '/chatrooms/H6Qw7eohRMNhoNoKGY0S/messages') // 아직 채팅방 여러개는 안됨
+          .orderBy('sendDate', descending: true)
+          .snapshots();
+
+      //스냅샷(Stream)내부의 자료들을 List<MessageModel> 로 변환하기 위해 map을 사용하도록 한다.
+      //참고로 List.map()도 List 안의 element들을 원하는 형태로 변환하여 새로운 List로 반환한다
+      return snapshots.map((querySnapshot) {
+        List<MessageModel> messages =
+            []; //querySnapshot을 message로 옮기기 위해 List<MessageModel> 선언
+        querySnapshot.docs.forEach((element) {
+          //해당 컬렉션에 존재하는 모든 docs를 순회하며 messages 에 데이터를 추가한다.
+          messages.add(MessageModel.fromMap(
+              id: element.id, map: element.data() as Map<String, dynamic>));
+        });
+        return messages; //QuerySnapshot에서 List<MessageModel> 로 변경이 됐으니 반환
+      }); //Stream<QuerySnapshot> 에서 Stream<List<MessageModel>>로 변경되어 반환됨
+
+    } catch (ex) {
+      //오류 발생 처리
+      log('error)', error: ex.toString(), stackTrace: StackTrace.current);
+      return Stream.error(ex.toString());
+    }
+  }
+
+  //전송버튼
   Widget getInputWidget() {
     return Container(
       height: 60,
@@ -112,49 +147,25 @@ class _MessageListScreenState extends State<MessageListScreen> {
     );
   }
 
+  //전송버튼을 눌렀을 때 동작
   void _onPressedSendButton() {
     try {
       //서버로 보낼 데이터를 모델클래스에 담아둔다.
-      //여기서 sendDate에 Timestamp.now()가 들어가는데 이는 디바이스의 시간을 나타내므로 나중에는 서버의 시간을 넣는 방법으로 변경하도록 하자.
-      MessageModel messageModel =
-          MessageModel(content: controller.text, sendDate: Timestamp.now());
+      MessageModel messageModel = MessageModel(
+          content: controller.text,
+          uid: userProvider.uid,
+          nickName: userProvider.nickname,
+          sendDate: Timestamp.now());
 
       //Firestore 인스턴스 가져오기
       FirebaseFirestore firestore = FirebaseFirestore.instance;
       //원하는 collection 주소에 새로운 document를 Map의 형태로 추가하는 모습.
       firestore
-          .collection('/chatrooms/2f0wsrh7TGnuOD8MI80f/messages')
+          .collection('/chatrooms/H6Qw7eohRMNhoNoKGY0S/messages')
           .add(messageModel.toMap());
+      controller.clear();
     } catch (ex) {
       log('error)', error: ex.toString(), stackTrace: StackTrace.current);
-    }
-  }
-
-  Stream<List<MessageModel>> streamMessages() {
-    try {
-      //찾고자 하는 컬렉션의 스냅샷(Stream)을 가져온다.
-      final Stream<QuerySnapshot> snapshots = FirebaseFirestore.instance
-          .collection('/chatrooms/2f0wsrh7TGnuOD8MI80f/messages') // 아직 채팅방 여러개는 안됨
-          .orderBy('sendDate')
-          .snapshots();
-
-      //새낭 스냅샷(Stream)내부의 자료들을 List<MessageModel> 로 변환하기 위해 map을 사용하도록 한다.
-      //참고로 List.map()도 List 안의 element들을 원하는 형태로 변환하여 새로운 List로 반환한다
-      return snapshots.map((querySnapshot) {
-        List<MessageModel> messages =
-            []; //querySnapshot을 message로 옮기기 위해 List<MessageModel> 선언
-        querySnapshot.docs.forEach((element) {
-          //해당 컬렉션에 존재하는 모든 docs를 순회하며 messages 에 데이터를 추가한다.
-          messages.add(MessageModel.fromMap(
-              id: element.id, map: element.data() as Map<String, dynamic>));
-        });
-        return messages; //QuerySnapshot에서 List<MessageModel> 로 변경이 됐으니 반환
-      }); //Stream<QuerySnapshot> 에서 Stream<List<MessageModel>>로 변경되어 반환됨
-
-    } catch (ex) {
-      //오류 발생 처리
-      log('error)', error: ex.toString(), stackTrace: StackTrace.current);
-      return Stream.error(ex.toString());
     }
   }
 }
