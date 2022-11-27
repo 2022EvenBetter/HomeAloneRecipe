b6dad8138a3439092e58bb11cf9cbb16231a666d
Minsu <syt07100@gmail.com>
2022-11-27 09:07:05 +0900

    Feat: filter&showGroupBuying

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -6,39 +6,45 @@ class Post {
   String? title;
   String? content;
   int? maxParticipants;
+  int? curParticipants;
   String? date;
   String? time;
   String? meetingPlace;
-  int? curParticipants;
+  String? writerName;
+  List<String>? userLocation;
 
-  Post(
-      {this.upperCategory,
-      this.lowerCategory,
-      this.title,
-      this.content,
-      this.maxParticipants,
-        this.curParticipants,
-      this.date,
-      this.time,
-      this.meetingPlace});
+  Post({
+    this.upperCategory,
+    this.lowerCategory,
+    this.title,
+    this.content,
+    this.maxParticipants,
+    this.curParticipants,
+    this.date,
+    this.time,
+    this.meetingPlace,
+    this.writerName,
+    this.userLocation,
+  });
 
   Post.fromJson(dynamic json) {
-    upperCategory = json['upperCategory'];
-    lowerCategory = json['lowerCategory'];
-    title = json['title'];
-    content = json['content'];
+    upperCategory = json['UpperCategory'];
+    lowerCategory = json['LowerCategory'];
+    title = json['Title'];
+    content = json['Content'];
     maxParticipants = json['maxParticipants'];
-    curParticipants=json['curParticipants'];
-    date = json['date'];
-    time = json['time'];
-    meetingPlace = json['meetingPlace'];
+    curParticipants = json['curParticipants'];
+    date = json['Date'];
+    time = json['Time'];
+    meetingPlace = json['Place'];
+    writerName = json['WriterName'];
+    userLocation = json['UserLocation'].cast<String>();
   }
 
   Post.fromSnapShot(DocumentSnapshot<Map<String, dynamic>> snapshot)
       : this.fromJson(snapshot.data());
 
-  Post.fromQuerySnapshot(
-      QueryDocumentSnapshot<Map<String, dynamic>> snapshot)
+  Post.fromQuerySnapshot(QueryDocumentSnapshot<Map<String, dynamic>> snapshot)
       : this.fromJson(snapshot.data());
 
   Map<String, dynamic> toJson() => {
@@ -51,5 +57,6 @@ class Post {
         'date': date,
         'time': time,
         'meetingPlace': meetingPlace,
+        'writerName': writerName,
       };
 }
