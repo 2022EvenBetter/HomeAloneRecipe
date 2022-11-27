c896b7958427270d478f83e432ca845ecda504b7
parkmingyun99 <griffinland9@naver.com>
2022-11-27 21:54:30 +0900

    feat:showFridge

---
+++ lib/screen/userPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({Key? key}) : super(key: key);
+
+  @override
+  _UserPage createState() => _UserPage();
+}
+
+class _UserPage extends State<UserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
